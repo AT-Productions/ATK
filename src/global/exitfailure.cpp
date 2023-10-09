c84ed0154314466da848b1ca3856996a67a6b238
antako <114558368+Antonako1@users.noreply.github.com>
2023-10-09 22:49:16 +0300

    Add exitfailure.cpp

---
+++ src/global/exitfailure.cpp
@@ -0,0 +1,13 @@
+#include "../../include/exitFailure.h"
+
+#include <stdlib.h> // ? REMOVE IN FUTURE ?
+/**
+ * ? REMOVE IN FUTURE ?
+ * returns EXIT_FAILURE
+*/
+int exitfailure(){
+    exit(1);
+
+    // ? REMOVE IN FUTURE ?
+    return EXIT_FAILURE;
+}
