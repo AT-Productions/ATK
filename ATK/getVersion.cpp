728ff52e83b1a5e47e056bce1aa76ea4c683a4e3
antako <114558368+Antonako1@users.noreply.github.com>
2023-10-26 22:05:25 +0300

    Hotfix 1.0.2

--- ATK/getVersion.cpp
+++ ATK/getVersion.cpp
@@ -6,6 +6,6 @@
  * when user asks it
 */
 string getVersion(){
-    const string version = "Current version for atk.exe: 1.0.0";
+    const string version = "Current version for atk.exe: 1.0.2";
     return version + "\n";
 }
