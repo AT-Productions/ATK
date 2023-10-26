9c0e678446d1b19383d0706b32c870cdc1f24fe1
antako <114558368+Antonako1@users.noreply.github.com>
2023-10-26 22:05:09 +0300

    Update help

--- ATK/longHelp.cpp
+++ ATK/longHelp.cpp
@@ -3,6 +3,6 @@
 #include "getVersion.h"
 
 string longHelp(){
-    string result = getVersion() + "atk [TYPE] [PATH] [PASSWORD] \n-----------------------------\nEXAMPLES \natk -t dir -p \"./dir/name/\" -s 1234 \natk -t file -p \"C:\\path\\to\\file.ext\\\" -s 4321 \n\nARG TYPES : \n -t = FILE TYPE | dir | file \n -p = PATH | .\\filename.ext | absolutepath \n -s = PASSWORD | anything | \"you want\"\n--help = Returns this message \n -v / --version = Returns current version \n -l / --location = Returns the location of atk.exe\n-----------------------------\n";
-        return result;
+    string result = getVersion() + "Usage: atk [TYPE] [PATH] [KEY]\n\n---------------------------\n\nEXAMPLES:\n\natk -t dir -p \"./dir/name/\" -k 1234\natk -t file -p \"C:\\path\\to\\file.ext\" -k 4321\n\n---------------------------\n\nARGUMENTS:\n\n-t, --type      FILE TYPE: Choose \'dir\' for directories or \'file\' for files.\n-p, --path      PATH: Specify the target path, either relative like \"./filename.ext\" or an absolute path.\n-k, --key       KEY: Set the key for encryption. It can be any string, enclosed in double quotes if needed.\n\n---------------------------\n\nOPTIONS:\n\n--help          Display this help message.\n-v, --version   Show the current version of atk.\n-l, --location  Return the location of the atk.exe executable.\n\n---------------------------\n";
+    return result;
 }
