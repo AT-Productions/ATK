40f5dbacb351a62245b4f956835e64d8b3e07a79
jooapa <jooa.akonpelto@gmail.com>
2023-11-04 23:49:15 +0200

    can be decrypted by double click

--- installation/admin/open_with_atk.bat
+++ installation/admin/open_with_atk.bat
@@ -1,3 +1,11 @@
 @echo off
-atk -t f -p "%1"
+set /p key="Enter the key: "
+if "%key%"=="" (
+    echo Key cannot be empty. Exiting.
+    exit /b 1
+)
+
+rem Replace the command below with your desired command, using %key% to insert the user-provided key.
+atk -t f -p "%1" -s -k "%key%"
+
 pause
