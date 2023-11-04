4ced72393f119c0d975cb4f3cb2619eebdd5087b
jooapa <jooa.akonpelto@gmail.com>
2023-11-05 00:29:43 +0200

    "paremmat" iconit

--- installation/admin/open_with_atk.bat
+++ installation/admin/open_with_atk.bat
@@ -1,11 +1,10 @@
 @echo off
-set /p key="Enter the key: "
+set /p key="Enter file key: "
 if "%key%"=="" (
-    echo Key cannot be empty. Exiting.
+    echo Error. Key is not defined.
     exit /b 1
 )
 
-rem Replace the command below with your desired command, using %key% to insert the user-provided key.
 atk -t f -p "%1" -s -k "%key%"
 
-pause
+exit
